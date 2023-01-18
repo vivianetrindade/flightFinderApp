763597cf9288d062d8bc88cfa114c6db810b137f
viviane.trindade <trindade.viviane@gmail.com>
2023-01-18 13:59:58 +0100

    added Mongo cluster on .env file.

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@sandbox.9ecwthe.mongodb.net/flights`;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.9ecwthe.mongodb.net/flights`;
 const AMADEUS_CLIENT_ID = process.env.AMADEUS_CLIENT_ID;
 const AMADEUS_CLIENT_SECRET = process.env.AMADEUS_CLIENT_SECRET;
 const port = process.env.PORT || 8000;
